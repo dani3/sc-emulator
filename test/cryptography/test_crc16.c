cbfe83b82d7eaed2f80bbf4726b58a0b8032f750
Dani M <daniel.m.aldea@hotmail.com>
2021-09-13 21:23:06 +0200

    Added more unit tests for CRC16

--- test/cryptography/test_crc16.c
+++ test/cryptography/test_crc16.c
@@ -7,6 +7,8 @@
 
 #include "unity.h"
 
+#include <string.h>
+
 #include "crc16.h"
 
 void setUp(void) {
@@ -28,4 +30,45 @@ void test_crc16_1_byte(void) {
 
   msg[0] = 0xA5;
   TEST_ASSERT_EQUAL_HEX16(0xE54F, crc16_calculate(msg, sizeof(msg)));
+}
+
+/**
+ * \brief Test that the CRC16 is calculated for a 2-byte message.
+ */
+void test_crc16_2_bytes(void) {
+  u8 msg[2];
+
+  msg[0] = 0x01;
+  msg[1] = 0x01;
+  TEST_ASSERT_EQUAL_HEX16(0x2310, crc16_calculate(msg, sizeof(msg)));
+
+  msg[0] = 0xA5;
+  msg[1] = 0xA5;
+  TEST_ASSERT_EQUAL_HEX16(0x07C4, crc16_calculate(msg, sizeof(msg)));
+}
+
+/**
+ * \brief Test that the CRC16 is calculated for an 8-byte message.
+ */
+void test_crc16_8_bytes(void) {
+  u8 msg[8];
+
+  memset(msg, 0x01, sizeof(msg));
+  TEST_ASSERT_EQUAL_HEX16(0x72D7, crc16_calculate(msg, sizeof(msg)));
+
+  memset(msg, 0xA5, sizeof(msg));
+  TEST_ASSERT_EQUAL_HEX16(0xDE7E, crc16_calculate(msg, sizeof(msg)));
+}
+
+/**
+ * \brief Test that the CRC16 is calculated for a 256-byte message.
+ */
+void test_crc16_256_bytes(void) {
+  u8 msg[256];
+
+  memset(msg, 0x01, sizeof(msg));
+  TEST_ASSERT_EQUAL_HEX16(0xD034, crc16_calculate(msg, sizeof(msg)));
+
+  memset(msg, 0xA5, sizeof(msg));
+  TEST_ASSERT_EQUAL_HEX16(0xE2D2, crc16_calculate(msg, sizeof(msg)));
 }
